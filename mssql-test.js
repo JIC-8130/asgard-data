caaf55e217ea2022dcc007475384ad6ccb05c643
Jahziel Villasana <jahzielv@gmail.com>
2019-01-12 12:22:49 -0500

    mssql now lets us store query rs in non-res object

--- mssql-test.js
+++ mssql-test.js
@@ -21,37 +21,56 @@ var server = testServer.listen(process.env.PORT || 8000, () =>
 
 const JSON_FRMT_STR_NO_WRAP = " for json path, without_array_wrapper";
 
-function executeQuery(result, query) {
+async function retrieveData(query) {
     sql.close();
-    sql.connect(
-        DB_CONFIG.testConfig,
-        err => {
-            if (err) {
-                console.log("ERROR: " + err);
-                result.send(err);
-            } else {
-                var request = new sql.Request();
-                request.query(query, (err, rs) => {
-                    if (err) {
-                        console.log("ERROR ON 33: " + err);
-                        result.send(err);
-                    } else {
-                        result.send(rs);
-                    }
-                });
-            }
-        }
-    );
+    // sql.connect(
+    //     DB_CONFIG.testConfig,
+    //     err => {
+    //         if (err) {
+    //             console.log("ERROR: " + err);
+    //             result.send(err);
+    //         } else {
+    //             var request = new sql.Request();
+    //             request.query(query, (err, rs) => {
+    //                 if (err) {
+    //                     console.log("ERROR ON 33: " + err);
+    //                     result.send(err);
+    //                 } else {
+    //                     result.send(rs.recordsets);
+    //                 }
+    //             });
+    //         }
+    //     }
+    // );
+    try {
+        let pool = await sql.connect(DB_CONFIG.testConfig);
+        let result = await pool
+            .request()
+            // .input()
+            .query(query);
+        return result;
+    } catch (err) {
+        return { db_err: err };
+    }
 }
 
-testServer.get("/users/:id", (req, res) => {
-    var userSelectSQL = jsonSQL.build({
-        type: "select",
-        table: "Users",
-        condition: [{ YCA_ID: { $eq: req.params.id } }]
+testServer.get("/users/:id", async (req, res) => {
+    // var userSelectSQL = jsonSQL.build({
+    //     type: "select",
+    //     table: "Users",
+    //     condition: [{ YCA_ID: { $eq: req.params.id } }]
+    // });
+
+    // console.log(userSelectSQL.query);
+    // let userResult;
+    let usrRes = await retrieveData(
+        `select * from "Users" where "YCA_ID" =${req.params.id}`
+    );
+    sql.on("error", err => {
+        res.text("<h1>That's a 500 server error ");
     });
-
-    console.log(userSelectSQL.query);
-
-    executeQuery(res, 'select * from "Users" where "YCA_ID" =' + req.params.id);
+    console.log(usrRes);
+    let response = { status: 200, data: usrRes.recordset[0] };
+    res.json(response);
+    // res.json("working");
 });
